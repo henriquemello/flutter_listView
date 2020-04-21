d47389967e4c61a85ded8f3fe830439e7aad952a
Henrique Mello <henriquemello@MacBook-Pro-de-Henrique.local>
2020-04-20 21:30:10 -0300

    adicionando dismissed component

--- flutter_list_view/lib/views/PrincipalView.dart
+++ flutter_list_view/lib/views/PrincipalView.dart
@@ -6,10 +6,10 @@ class PrincipalView extends StatefulWidget {
 
   PrincipalView() {
     items = [];
-    items.add(Item("BCFF11",false));
-    items.add(Item("HGCR11",true));
-    items.add(Item("RNDP11",false));
-    items.add(Item("XPTO11",false));
+    items.add(Item("BCFF11", false));
+    items.add(Item("HGCR11", true));
+    items.add(Item("RNDP11", false));
+    items.add(Item("XPTO11", false));
   }
 
   @override
@@ -17,30 +17,70 @@ class PrincipalView extends StatefulWidget {
 }
 
 class _PrincipalViewState extends State<PrincipalView> {
+  TextEditingController tarefaController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Listagem"),
-          centerTitle: true,
+      appBar: AppBar(
+        title: TextFormField(
+          keyboardType: TextInputType.text,
+          controller: tarefaController,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+          decoration: InputDecoration(
+              labelText: "Nova Tarefa",
+              labelStyle: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              )),
         ),
-        body: ListView.builder(
-          itemCount: widget.items.length,
-          itemBuilder: (context, index) {
-                    
-            final item = widget.items[index];
-            return CheckboxListTile(
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (context, index) {
+          final item = widget.items[index];
+
+          return Dismissible(
+            child: CheckboxListTile(
               title: Text(item.title),
               value: item.done,
-              key: Key(item.title),
-              onChanged: (value){
+              onChanged: (value) {
                 setState(() {
                   item.done = value;
                 });
               },
-            );
-          },
+            ),
+            key: Key(item.title),
+            background: Container(color: Colors.pink.withOpacity(0.2)),
+            onDismissed: (direction) {
+              print(direction);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _add(tarefaController.text);
+        },
+        backgroundColor: Colors.pink,
+        child: Icon(
+          Icons.add,
         ),
-      );
+      ),
+    );
+  }
+
+  void _add(String text) {
+    if (text.isEmpty) return;
+
+    Item item = Item(text, false);
+    setState(() {
+      widget.items.add((item));
+    });
+    tarefaController.clear();
   }
 }
